0e52a1f0d664f13b77c705fd88794cbd8a16865e
mndankova <veritas.dss@gmail.com>
2017-09-29 01:35:32 +0300

    Update signinPage.js

--- signinPage.js
+++ signinPage.js
@@ -8,113 +8,30 @@ var signinPage = function () {
         };
 
         this.email = element(by.name('email'));
-
-        this.setEmail = function(value) {
-            this.email.clear();
-            this.email.sendKeys(value);
-        };
-
-        this.shouldHaveEmail = function(value) {
-            expect(this.email.getAttribute('value')).toEqual(value);
-        };
-
-        this.emailShouldBeVisible = function() {
-            expect(this.email.isDisplayed()).toBeTruthy();
-        };
-
-        this.emailShouldNotBeVisible = function() {
-            expect(this.email.isDisplayed()).toBeFalsy();
-        };
-
-        this.emailShouldBeEnabled = function() {
-            expect(this.email.isEnabled()).toBeTruthy();
-        };
-
-        this.emailShouldNotBeEnabled = function() {
-            expect(this.email.isEnabled()).toBeFalsy();
-        };
+        this.setEmail = function(value) {this.email.clear(); this.email.sendKeys(value);};
+        this.shouldHaveEmail = function(value) {expect(this.email.getAttribute('value')).toEqual(value);};
+        this.emailShouldBeVisible = function() {expect(this.email.isDisplayed()).toBeTruthy();};
+        this.emailShouldBeEnabled = function() {expect(this.email.isEnabled()).toBeTruthy();};
 
         this.password = element(by.name('password'));
-
-        this.setPassword = function(value) {
-            this.password.clear();
-            this.password.sendKeys(value);
-        };
-
-        this.shouldHavePassword = function(value) {
-            expect(this.password.getAttribute('value')).toEqual(value);
-        };
-
-        this.passwordShouldBeVisible = function() {
-            expect(this.password.isDisplayed()).toBeTruthy();
-        };
-
-        this.passwordShouldNotBeVisible = function() {
-            expect(this.password.isDisplayed()).toBeFalsy();
-        };
-
-        this.passwordShouldBeEnabled = function() {
-            expect(this.password.isEnabled()).toBeTruthy();
-        };
-
-        this.passwordShouldNotBeEnabled = function() {
-            expect(this.password.isEnabled()).toBeFalsy();
-        };
-
+        this.setPassword = function(value) {this.password.clear(); this.password.sendKeys(value);};
+        this.shouldHavePassword = function(value) {expect(this.password.getAttribute('value')).toEqual(value);};
+        this.passwordShouldBeVisible = function() {expect(this.password.isDisplayed()).toBeTruthy();};
+        this.passwordShouldBeEnabled = function() {expect(this.password.isEnabled()).toBeTruthy();};
+      
         this.code = element(by.name('code'));
-
-        this.setCode = function(value) {
-            this.code.clear();
-            this.code.sendKeys(value);
-        };
-
-        this.shouldHaveCode = function(value) {
-            expect(this.code.getAttribute('value')).toEqual(value);
-        };
-
-        this.codeShouldBeVisible = function() {
-            expect(this.code.isDisplayed()).toBeTruthy();
-        };
-
-        this.codeShouldNotBeVisible = function() {
-            expect(this.code.isDisplayed()).toBeFalsy();
-        };
-
-        this.codeShouldBeEnabled = function() {
-            expect(this.code.isEnabled()).toBeTruthy();
-        };
-
-        this.codeShouldNotBeEnabled = function() {
-            expect(this.code.isEnabled()).toBeFalsy();
-        };
-
+        this.setCode = function(value) {this.code.clear(); this.code.sendKeys(value);};
+        this.shouldHaveCode = function(value) {expect(this.code.getAttribute('value')).toEqual(value);};
+        this.codeShouldBeVisible = function() {expect(this.code.isDisplayed()).toBeTruthy();};       
+        this.codeShouldBeEnabled = function() {expect(this.code.isEnabled()).toBeTruthy();};
+        
         this.logInButton = element(by.buttonText('Log in'));
-
-        this.clickLogInButton = function() {
-            this.logInButton.click();
-        };
-
-        this.logInButtonShouldBeVisible = function() {
-            expect(this.logInButton.isDisplayed()).toBeTruthy();
-        };
-
-        this.logInButtonShouldNotBeVisible = function() {
-            expect(this.logInButton.isDisplayed()).toBeFalsy();
-        };
-
+        this.clickLogInButton = function() {this.logInButton.click();};
+        this.logInButtonShouldBeVisible = function() {expect(this.logInButton.isDisplayed()).toBeTruthy();};
+        
         this.signUpLink = element(by.linkText('Sign Up'));
-
-        this.clickSignUpLink = function() {
-            this.signUpLink.click();
-        };
-
-        this.signUpLinkShouldBeVisible = function() {
-            expect(this.signUpLink.isDisplayed()).toBeTruthy();
-        };
-
-        this.signUpLinkShouldNotBeVisible = function() {
-            expect(this.signUpLink.isDisplayed()).toBeFalsy();
-       };
+        this.clickSignUpLink = function() {this.signUpLink.click();};
+        this.signUpLinkShouldBeVisible = function() {expect(this.signUpLink.isDisplayed()).toBeTruthy();};
     }
  };
 
